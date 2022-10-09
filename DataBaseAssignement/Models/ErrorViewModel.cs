bf194b38ef00d082a80396d3e0f42e2f4ad2feed
fadiabji <fadi.abji@htomail.com>
2022-10-09 16:11:16 +0200

    Add project files.

---
+++ DataBaseAssignement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataBaseAssignement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
